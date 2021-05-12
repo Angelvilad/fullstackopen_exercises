203ad6ac8ed0321d6b9323393f3cfd39bc5a327d
Angelvilad <angelvilad@hotmail.com>
2021-05-13 00:28:21 +0200

    Exercise 4.2 completed

--- part4/blog_list/index.js
+++ part4/blog_list/index.js
@@ -1,44 +1,11 @@
-require('dotenv').config();
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app.js');
+const http = require('http');
+const config = require('./utils/config.js');
+const logger = require('./utils/logger.js');
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const server = http.createServer(app);
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGO_DB_URI;
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+const port = config.PORT || 3003;
+server.listen(port, () => {
+  logger.info(`Server running on portaaaa ${port}`)
+});
