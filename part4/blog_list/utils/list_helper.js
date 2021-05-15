621ec4b5736405188c5e01aa74b60642f3883064
Angelvilad <angelvilad@hotmail.com>
2021-05-15 08:35:32 +0200

    Exercise 4.4 completed

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -2,6 +2,21 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) return 0;
+  
+  const reducer = (sum, item) => {
+    const likes = item.likes;
+
+    return sum + likes;
+  }
+
+  const totalLikes = blogs.reduce(reducer, 0);
+
+  return totalLikes;
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
