34c93cd150b5548f7456a1a95ab9c73f706b5e15
Angelvilad <angelvilad@hotmail.com>
2021-05-19 22:22:11 +0200

    Exercise 4.7 completed

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -18,7 +18,7 @@ const totalLikes = (blogs) => {
 
 const favoriteBlog = (blogs) => {
   if(blogs.length === 0) return 0;
-  
+
   const newBlogs = [...blogs];
   newBlogs.sort((a,b) => b.likes - a.likes);
   const favoriteBlog = newBlogs[0];
@@ -28,8 +28,50 @@ const favoriteBlog = (blogs) => {
   return {title, author, likes};
 }
 
+const mostBlogs = (blogs) => {
+  if(blogs.length === 0) return 0;
+
+  const totalBlogsByAuthors = {};
+
+  blogs.forEach(blog => {
+    if(!totalBlogsByAuthors[blog.author]) return totalBlogsByAuthors[blog.author] = 1;
+
+    ++totalBlogsByAuthors[blog.author];
+  });
+
+  const authorWithMostBlogs = Object.keys(totalBlogsByAuthors).reduce((a, b) => (totalBlogsByAuthors[a] > totalBlogsByAuthors[b]) ? a : b);
+
+
+  return ({
+    author: authorWithMostBlogs,
+    blogs: totalBlogsByAuthors[authorWithMostBlogs]
+  });
+
+}
+
+const mostLikes = (blogs) => {
+  if(blogs.length === 0) return 0;
+  
+  const totalLikesByAuthors = {};
+
+  blogs.forEach(blog => {
+    if(!totalLikesByAuthors[blog.author]) return totalLikesByAuthors[blog.author] = blog.likes;
+
+    totalLikesByAuthors[blog.author] += blog.likes;
+  });
+
+  const authorWithMostLikes = Object.keys(totalLikesByAuthors).reduce((a, b) => (totalLikesByAuthors[a] > totalLikesByAuthors[b]) ? a : b);
+
+  return ({
+    author: authorWithMostLikes,
+    likes: totalLikesByAuthors[authorWithMostLikes]
+  });
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 };
